03fa3e73abaa1a14f5ecd0db910e8be8746700db
comrang-altf4 <53970293+comrang-altf4@users.noreply.github.com>
2021-03-30 15:34:44 +0700

    Update fitness + export picture

--- main.py
+++ main.py
@@ -3,58 +3,36 @@ import random
 import imutils
 from scipy import spatial
 import scipy.sparse
+import numpy as np
 n = 3  # size of individual (chromosome)
 m = 100  # size of population
-n_generations = 40  # number of generations
+n_generations = 40 # number of generations
 fitnesses = []
-
-def generate_random_value():
-    return ([random.randint(0, src.shape[0]),random.randint(0, src.shape[1]),random.randint(0, 360)])
 def crop_img(individual,rotated):
     h = int(rotated.shape[0] / 2)
     w = int(rotated.shape[1] / 2)
-    if individual[0] - h < 0:
-        h = individual[0]
-    elif individual[0] + h > src.shape[0]:
-        h = src.shape[0] - individual[0]
-    if individual[1] - w < 0:
-        w = individual[1]
-    elif individual[1] + w > src.shape[1]:
-        w = src.shape[1] - individual[1]
-    crop = src[individual[0] - h:individual[0] + h, individual[1] - w:individual[1] + w]
-    top = 0
-    bottom = top
-    left = 0  # shape[1] = cols
-    right = left
-    if crop.shape[0] < rotated.shape[0]:
-        h = rotated.shape[0] - crop.shape[0]
-        top = int(h / 2)
-        bottom = int(h / 2)
-        if h % 2 == 1:
-            bottom = int(h / 2) + 1
-    if crop.shape[1] < rotated.shape[1]:
-        h = rotated.shape[1] - crop.shape[1]
-        left = int(h / 2)
-        right = int(h / 2)
-        if h % 2 == 1:
-            right = int(h / 2) + 1
-    borderType = cv2.BORDER_CONSTANT
-    crop = cv2.copyMakeBorder(crop, top, bottom, left, right, borderType, None, 0)
-    ret, crop = cv2.threshold(crop, 127, 255, cv2.THRESH_BINARY)
+    t_h=0
+    t_w=0
+    if rotated.shape[0]%2==1:
+        t_h=1
+    if rotated.shape[1]%2==1:
+        t_w=1
+    crop = src[individual[0] - h:individual[0] + h+t_h, individual[1] - w:individual[1] + w+t_w]
     return crop
 def compute_fitness(individual):
     rotated = imutils.rotate_bound(image, individual[2])
     ret,rotated = cv2.threshold(rotated, 127, 255, cv2.THRESH_BINARY)
     crop = crop_img(individual,rotated)
     ret, crop = cv2.threshold(crop, 127, 255, cv2.THRESH_BINARY)
-
-    sA = scipy.sparse.csr_matrix(crop.flatten())
-    sB = scipy.sparse.csr_matrix(rotated.flatten())
-    return spatial.distance.cosine(sA.toarray(), sB.toarray())
-
-
+    sA = scipy.sparse.csr_matrix(crop.flatten()).toarray()
+    sB = scipy.sparse.csr_matrix(rotated.flatten()).toarray()
+    if 255 not in sA[0]:
+        ttt=1
+    else:
+        ttt = spatial.distance.cosine(sA, sB)
+    return 1-ttt
 def create_individual():
-    return ([random.randint(0, src.shape[0]), random.randint(0, src.shape[1]), random.randint(0, 360)])
+    return ([random.randint(int(h_max/2), src.shape[0]-int(h_max)),random.randint(int(w_max/2) , src.shape[1]-int(w_max)),random.randint(0, 360)])
 
 def crossover(individual1, individual2, crossover_rate=0.9):
     individual1_new = individual1.copy()
@@ -68,17 +46,18 @@ def crossover(individual1, individual2, crossover_rate=0.9):
     return individual1_new, individual2_new
 
 
-def mutate(individual, mutation_rate=0.05):
+def mutate(individual, mutation_rate=0.07):
     individual_m = individual.copy()
 
     for i in range(n):
+        temp=create_individual()
         if random.random() < mutation_rate:
             if i==0:
-                individual_m[i] = random.randint(0, src.shape[0])
+                individual_m[i] = temp[0]
             elif i==1:
-                individual_m[i]=random.randint(0, src.shape[1])
+                individual_m[i]=temp[1]
             else:
-                individual_m[i]=random.randint(0, 360)
+                individual_m[i]=temp[2]
     return individual_m
 
 
@@ -89,18 +68,20 @@ def selection(sorted_old_population):
         if (index2 != index1):
             break
 
-    individual_s = sorted_old_population[index2]
+    individual_s = sorted_old_population[index1]
     if index2 > index1:
-        individual_s = sorted_old_population[index1]
+        individual_s = sorted_old_population[index2]
 
     return individual_s
 
 
 def create_new_population(old_population, elitism=2, gen=1):
     sorted_population = sorted(old_population, key=compute_fitness)
+
     if gen % 1 == 0:
-        fitnesses.append(compute_fitness(sorted_population[0]))
-        print("BEST:", compute_fitness(sorted_population[0]))
+        fitnesses.append(compute_fitness(sorted_population[m - 1]))
+        print("BEST:", compute_fitness(sorted_population[m - 1]))
+
     new_population = []
     while len(new_population) < m - elitism:
         # selection
@@ -120,21 +101,33 @@ def create_new_population(old_population, elitism=2, gen=1):
     for ind in sorted_population[m - elitism:]:
         new_population.append(ind.copy())
 
-    return sorted(new_population,key=compute_fitness)
+    return new_population
 
-c=0
+
 image = cv2.imread("template_crop.png",0)
+w_max=0
+h_max=0
+for i in range(360):
+    temp=imutils.rotate_bound(image,i)
+    w_max=max(w_max,temp.shape[1])
+    h_max=max(h_max,temp.shape[0])
 src=cv2.imread("image1.jpg",0)
+borderType = cv2.BORDER_CONSTANT
+src = cv2.copyMakeBorder(src, h_max, h_max, w_max, w_max, borderType, None, 0)
 population = [create_individual() for _ in range(m)]
-#print(population)
+for i in population:
+    if i[0]+h_max>src.shape[0] or i[1]+w_max>src.shape[1]:
+        print(i)
 for i in range(n_generations):
     population = create_new_population(population, 2, i)
-#print(population)
+
 individual=population[0]
 rotated = imutils.rotate_bound(image, individual[2])
 ret,rotated = cv2.threshold(rotated, 127, 255, cv2.THRESH_BINARY)
 crop = crop_img(individual,rotated)
-ret,crop = cv2.threshold(crop, 127, 255, cv2.THRESH_BINARY)
 cv2.imshow('a',crop)
 cv2.imshow('b',rotated)
+cv2.rectangle(src, (individual[1]-int(crop.shape[1]/2),individual[0]-int(crop.shape[0]/2)), (individual[1] + int(crop.shape[1]/2),individual[0]+int(crop.shape[0]/2)), (200, 0, 0), 3)
+cv2.imwrite("found.jpg",src)
+
 cv2.waitKey()
