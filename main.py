566296164316e4832884eff32e4e4f60a2c580ef
comrang-altf4 <53970293+comrang-altf4@users.noreply.github.com>
2021-04-03 09:54:41 +0700

    Delete obj

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import scipy.sparse
 import numpy as np
 n = 3  # size of individual (chromosome)
 m = 100  # size of population
-n_generations = 40 # number of generations
+n_generations = 10 # number of generations
 fitnesses = []
 def crop_img(individual,rotated):
     h = int(rotated.shape[0] / 2)
@@ -17,7 +17,7 @@ def crop_img(individual,rotated):
         t_h=1
     if rotated.shape[1]%2==1:
         t_w=1
-    crop = src[individual[0] - h:individual[0] + h+t_h, individual[1] - w:individual[1] + w+t_w]
+    crop = src2[individual[0] - h:individual[0] + h+t_h, individual[1] - w:individual[1] + w+t_w]
     return crop
 def compute_fitness(individual):
     rotated = imutils.rotate_bound(image, individual[2])
@@ -29,7 +29,7 @@ def compute_fitness(individual):
     if 255 not in sA[0]:
         ttt=1
     else:
-        ttt = spatial.distance.cosine(sA, sB)
+        ttt = spatial.distance.(sA, sB)
     return 1-ttt
 def create_individual():
     return ([random.randint(int(h_max/2), src.shape[0]-int(h_max)),random.randint(int(w_max/2) , src.shape[1]-int(w_max)),random.randint(0, 360)])
@@ -46,7 +46,9 @@ def crossover(individual1, individual2, crossover_rate=0.9):
     return individual1_new, individual2_new
 
 
-def mutate(individual, mutation_rate=0.07):
+def mutate(individual, mutation_rate=0.4):
+    if geni-n_generations>0:
+        mutation_rate=0.1*int((geni-n_generations)/10+1)
     individual_m = individual.copy()
 
     for i in range(n):
@@ -74,7 +76,94 @@ def selection(sorted_old_population):
 
     return individual_s
 
-
+def remove(crop,rotated):
+    h = int(rotated.shape[0] / 2)
+    w = int(rotated.shape[1] / 2)
+    t_h = 0
+    t_w = 0
+    if rotated.shape[0] % 2 == 1:
+        t_h = 1
+    if rotated.shape[1] % 2 == 1:
+        t_w = 1
+    for i in range(crop.shape[0]):
+        for j in range(crop.shape[1]):
+            if crop[i,j]==rotated[i,j] and crop[i,j]!=0:
+                crop[i,j]=0
+    src2[individual[0] - h:individual[0] + h + t_h, individual[1] - w:individual[1] + w + t_w]=crop
+    return 0
+def bfs(x,y,mode):
+    bfs_coord=[[x,y]]
+    l=0
+    bfs_bool_arr=[[True for i in range(src.shape[0])] for j in range(src.shape[1])]
+    temp=src2[x,y]
+    bfs_bool_arr[x][y]=False
+    if temp!=0 and mode==0:
+        return x,y
+    while l<=len(bfs_coord)-1:
+        x,y=bfs_coord[l]
+        for skamtt in range(6):
+            skamt=skamtt+1
+            if bfs_bool_arr[x+skamt][y]==True:
+                if src2[x+skamt,y]!=0:
+                    if mode==0:
+                        return x+skamt,y
+                    else:
+                        src2[x+skamt,y]=0
+                        bfs_coord.append([x + skamt, y])
+                else:
+                    if mode==0:
+                        bfs_coord.append([x + skamt, y])
+                bfs_bool_arr[x + skamt][y]=False
+            if bfs_bool_arr[x-skamt][y]==True:
+                if src2[x - skamt, y] != 0:
+                    if mode == 0:
+                        return x - skamt, y
+                    else:
+                        src2[x - skamt, y] = 0
+                        bfs_coord.append([x - skamt, y])
+                else:
+                    if mode == 0:
+                        bfs_coord.append([x - skamt, y])
+                bfs_bool_arr[x - skamt][y] = False
+            if bfs_bool_arr[x][y+skamt]==True:
+                if src2[x, y+1] != 0:
+                    if mode == 0:
+                        return x, y+skamt
+                    else:
+                        src2[x , y+ 1] = 0
+                        bfs_coord.append([x , y+ skamt])
+                else:
+                    if mode == 0:
+                        bfs_coord.append([x , y+ skamt])
+                bfs_bool_arr[x][y + skamt] = False
+            if bfs_bool_arr[x][y-skamt]==True:
+                if src2[x , y-skamt] != 0:
+                    if mode == 0:
+                        return x, y-skamt
+                    else:
+                        src2[x , y-skamt] = 0
+                        bfs_coord.append([x, y-skamt])
+                else:
+                    if mode == 0:
+                        bfs_coord.append([x , y-skamt])
+                bfs_bool_arr[x ][y-skamt] = False
+        l+=1
+def remove2(individual,rotated):
+    h = int(rotated.shape[0] / 2)
+    w = int(rotated.shape[1] / 2)
+    t_h = 0
+    t_w = 0
+    if rotated.shape[0] % 2 == 1:
+        t_h = 1
+    if rotated.shape[1] % 2 == 1:
+        t_w = 1
+    x_t=individual[0]+int(h/2)+t_h
+    y_t=individual[1]+int(w/2)+t_w
+    print(x_t,y_t)
+    x_t,y_t=bfs(x_t,y_t,0)
+    print(src2[x_t,y_t])
+    print(x_t,y_t)
+    bfs(x_t,y_t,1)
 def create_new_population(old_population, elitism=2, gen=1):
     sorted_population = sorted(old_population, key=compute_fitness)
 
@@ -111,23 +200,35 @@ for i in range(360):
     temp=imutils.rotate_bound(image,i)
     w_max=max(w_max,temp.shape[1])
     h_max=max(h_max,temp.shape[0])
-src=cv2.imread("image1.jpg",0)
+src=cv2.imread("template2.jpg",0)
 borderType = cv2.BORDER_CONSTANT
 src = cv2.copyMakeBorder(src, h_max, h_max, w_max, w_max, borderType, None, 0)
-population = [create_individual() for _ in range(m)]
-for i in population:
-    if i[0]+h_max>src.shape[0] or i[1]+w_max>src.shape[1]:
-        print(i)
-for i in range(n_generations):
-    population = create_new_population(population, 2, i)
+src2 = src
+ret, src2 = cv2.threshold(src2, 127, 255, cv2.THRESH_BINARY)
+geni=0
+for k in range(13):
+    population = [create_individual() for _ in range(m)]
 
-individual=population[0]
-rotated = imutils.rotate_bound(image, individual[2])
-ret,rotated = cv2.threshold(rotated, 127, 255, cv2.THRESH_BINARY)
-crop = crop_img(individual,rotated)
-cv2.imshow('a',crop)
-cv2.imshow('b',rotated)
-cv2.rectangle(src, (individual[1]-int(crop.shape[1]/2),individual[0]-int(crop.shape[0]/2)), (individual[1] + int(crop.shape[1]/2),individual[0]+int(crop.shape[0]/2)), (200, 0, 0), 3)
-cv2.imwrite("found.jpg",src)
+    for i in population:
+        if i[0]+h_max>src.shape[0] or i[1]+w_max>src.shape[1]:
+            print(i)
+    geni=0
+    while geni<n_generations or compute_fitness(population[m-1])<0.5:
+        population = create_new_population(population, 2, geni)
+        geni+=1
+        if geni>n_generations+40:
+            break
 
-cv2.waitKey()
+    individual=population[m-1]
+    rotated = imutils.rotate_bound(image, individual[2])
+    ret,rotated = cv2.threshold(rotated, 127, 255, cv2.THRESH_BINARY)
+    crop = crop_img(individual,rotated)
+    ret,crop=cv2.threshold(crop, 127, 255, cv2.THRESH_BINARY)
+    # cv2.imshow('b',rotated)
+    #remove(crop,rotated)
+    remove2(individual,rotated)
+    #cv2.rectangle(src, (individual[1]-int(crop.shape[1]/2),individual[0]-int(crop.shape[0]/2)), (individual[1] + int(crop.shape[1]/2),individual[0]+int(crop.shape[0]/2)), (200, 0, 0), 3)
+    cv2.imwrite("found{}.jpg".format(k),src2)
+    print("{} iteration".format(k+1))
+    # cv2.imshow('a', crop)
+    # cv2.waitKey()
